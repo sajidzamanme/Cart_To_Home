4ee6fa85902b5f324b3c46884aefe2345c893d1e
Sajid <sajidzaman.2002.3@gmail.com>
2025-05-19 00:06:07 +0600

    Refactor SearchBar component layout and styling; add conditional rendering for clear button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { MdOutlineRemoveCircle } from "react-icons/md";
 
 const SearchBar = ({ searchLine, setSearchLine }) => {
   return (
-    <div className="w-full px-3">
+    <div className="flex flex-row items-center w-full h-[3rem] px-3 bg-[#f0f3fa] border-2 border-gray-500 rounded-lg">
       <input
         value={searchLine}
         type="text"
-        className="w-full h-[3rem] bg-[#f0f3fa] border-2 border-gray-500 rounded-lg p-2 placeholder:text-[#3F72AF]"
+        className="w-full pl-2 placeholder:text-[#3F72AF] border-0 outline-none"
         placeholder="Type to search catalogue"
         onChange={(e) => setSearchLine(e.target.value)}
       />
+
+      {searchLine !== "" && (
+        <button className="flex flex-row items-center justify-center px-2" onClick={() => setSearchLine("")}>
+          <MdOutlineRemoveCircle className="text-[#112D4E] size-5" />
+        </button>
+      )}
     </div>
   );
 };
